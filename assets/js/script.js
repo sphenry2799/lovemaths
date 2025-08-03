04539fbb4cd881db0ac6db4efe126bdc68773835
sphenry2799 <sp.henry2799@gmail.com>
2025-08-03 18:39:04 +0100

    Add event listeners and get attributes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
 
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
+
+function runGame() {}
+
+function checkAnswer() {}
+
+function calculatecorrectAnswer() {}
+
+function incrementScore() {}
+function incrementwrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
